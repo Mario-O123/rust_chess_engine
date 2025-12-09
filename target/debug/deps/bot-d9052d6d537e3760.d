8bcbf16483c66d680e99ce431833386f9c629c4b
Julien0905 <juju.kriebel11@gmail.com>
2025-12-09 14:38:45 +0100

    Added the Mailbox120 and 64 and implemented helper functions

---
+++ target/debug/deps/bot-d9052d6d537e3760.d
@@ -0,0 +1,5 @@
+/home/maju/RUST/rust_chess_engine/target/debug/deps/bot-d9052d6d537e3760.d: src/bin/bot.rs
+
+/home/maju/RUST/rust_chess_engine/target/debug/deps/libbot-d9052d6d537e3760.rmeta: src/bin/bot.rs
+
+src/bin/bot.rs:
