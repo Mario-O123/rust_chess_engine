e5fec4b4ba58641c6d176d730be0a0319fc6f027
Julien0905 <juju.kriebel11@gmail.com>
2025-12-09 13:59:24 +0100

    MAILBOX120 und 64 implementiert + Funktionen

---
+++ target/debug/deps/rust_chess_engine-fdc305e18e7b8ecb.d
@@ -0,0 +1,5 @@
+/home/maju/RUST/rust_chess_engine/target/debug/deps/rust_chess_engine-fdc305e18e7b8ecb.d: src/lib.rs
+
+/home/maju/RUST/rust_chess_engine/target/debug/deps/librust_chess_engine-fdc305e18e7b8ecb.rmeta: src/lib.rs
+
+src/lib.rs:
