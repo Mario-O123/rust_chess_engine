9b1703900ea5268b3ed22522ea76fc9a57733ce1
xxrail13 <noah.schuller@gmail.com>
2025-12-12 17:29:37 +0100

    added Features and fixed some heavy bugs

--- src/position/position.rs
+++ src/position/position.rs
@@ -1,3 +1,5 @@
+use crate::board::mailbox120::SQUARE120_TO_SQUARE64;
+use once_cell::sync::Lazy;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 
@@ -14,6 +16,20 @@ impl Color {
             Color::White => Color::Black,
         }
     }
+
+    pub fn idx(&self) -> u8 {
+        match self {
+            Color::White => 0,
+            Color::Black => 1,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Piece(Piece),
+    Offboard,
+    Empty,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -24,6 +40,19 @@ pub enum PieceKind {
     Rook,
     Queen,
     King,
+}
+
+impl PieceKind {
+    pub fn idx(self) -> u8 {
+        match self {
+            PieceKind::Pawn => 0,
+            PieceKind::Knight => 1,
+            PieceKind::Bishop => 2,
+            PieceKind::Rook => 3,
+            PieceKind::Queen => 4,
+            PieceKind::King => 5,
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -40,18 +69,18 @@ impl Piece {
 
 pub type Square = u8;
 
-// zobrist_values: [[[Squares] Piecekinds] Colors]
+// zobrist_values: [[[Squares]; Piecekinds]; Colors]
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct Zobrist {
     pub zobrist_values: [[[u64; 64]; 6]; 2],
     pub zobrist_side_to_move: u64,
     pub zobrist_castling: [u64; 4],
-    pub zobrist_enpassant_row: [u64; 8],
+    pub zobrist_enpassant: [u64; 8],
 }
 
 impl Zobrist {
     // Same Seed generates same random u64 Numbers everytime function is called
-    pub fn new_zobrist() -> Self {
+    pub fn init_zobrist() -> Self {
         const SEED: u64 = 42;
         let mut rng = StdRng::seed_from_u64(SEED);
         let mut zobrist_values = [[[0u64; 64]; 6]; 2];
@@ -71,154 +100,182 @@ impl Zobrist {
             zobrist_castling[i] = rng.r#gen();
         }
 
-        let mut zobrist_enpassant_row = [0u64; 8];
+        let mut zobrist_enpassant = [0u64; 8];
         for i in 0..8 {
-            zobrist_enpassant_row[i] = rng.r#gen();
+            zobrist_enpassant[i] = rng.r#gen();
         }
 
         Self {
-            zobrist_values: zobrist_values,
-            zobrist_side_to_move: zobrist_side_to_move,
-            zobrist_castling: zobrist_castling,
-            zobrist_enpassant_row: zobrist_enpassant_row,
+            zobrist_values,
+            zobrist_side_to_move,
+            zobrist_castling,
+            zobrist_enpassant,
         }
     }
 }
 
-pub const START_BOARD: [Option<Piece>; 64] = [
-    Some(Piece::new(Color::Black, PieceKind::Rook)),
-    Some(Piece::new(Color::Black, PieceKind::Knight)),
-    Some(Piece::new(Color::Black, PieceKind::Bishop)),
-    Some(Piece::new(Color::Black, PieceKind::Queen)),
-    Some(Piece::new(Color::Black, PieceKind::King)),
-    Some(Piece::new(Color::Black, PieceKind::Bishop)),
-    Some(Piece::new(Color::Black, PieceKind::Knight)),
-    Some(Piece::new(Color::Black, PieceKind::Rook)),
-    Some(Piece::new(Color::Black, PieceKind::Pawn)),
-    Some(Piece::new(Color::Black, PieceKind::Pawn)),
-    Some(Piece::new(Color::Black, PieceKind::Pawn)),
-    Some(Piece::new(Color::Black, PieceKind::Pawn)),
-    Some(Piece::new(Color::Black, PieceKind::Pawn)),
-    Some(Piece::new(Color::Black, PieceKind::Pawn)),
-    Some(Piece::new(Color::Black, PieceKind::Pawn)),
-    Some(Piece::new(Color::Black, PieceKind::Pawn)),
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    None,
-    Some(Piece::new(Color::White, PieceKind::Pawn)),
-    Some(Piece::new(Color::White, PieceKind::Pawn)),
-    Some(Piece::new(Color::White, PieceKind::Pawn)),
-    Some(Piece::new(Color::White, PieceKind::Pawn)),
-    Some(Piece::new(Color::White, PieceKind::Pawn)),
-    Some(Piece::new(Color::White, PieceKind::Pawn)),
-    Some(Piece::new(Color::White, PieceKind::Pawn)),
-    Some(Piece::new(Color::White, PieceKind::Pawn)),
-    Some(Piece::new(Color::White, PieceKind::Rook)),
-    Some(Piece::new(Color::White, PieceKind::Knight)),
-    Some(Piece::new(Color::White, PieceKind::Bishop)),
-    Some(Piece::new(Color::White, PieceKind::Queen)),
-    Some(Piece::new(Color::White, PieceKind::King)),
-    Some(Piece::new(Color::White, PieceKind::Bishop)),
-    Some(Piece::new(Color::White, PieceKind::Knight)),
-    Some(Piece::new(Color::White, PieceKind::Rook)),
-];
+// Blocks Memory for Zobrist. init_zobrist is called, when needed
+pub static ZOBRIST: Lazy<Zobrist> = Lazy::new(|| Zobrist::init_zobrist());
 
 // castling_rights uses 4 bits: White 0-0 (0b0001), White 0-0-0 (0b0010),
 // Black 0-0 (0b0100), Black 0-0-0 (0b1000)
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct Position {
-    pub board: [Option<Piece>; 64],
+    pub board: [Cell; 120],
     pub player_to_move: Color,
     pub en_passant_square: Option<Square>,
     pub castling_rights: u8,
     pub zobrist: u64,
-    pub half_move_clock: u8,
+    pub half_move_clock: u16,
     pub move_counter: u16,
-    pub zobrist_object: Zobrist,
 }
 
 impl Position {
     fn empty() -> Self {
         Self {
-            board: [None; 64],
+            board: Self::init_empty_board(),
             player_to_move: Color::White,
             en_passant_square: None,
             castling_rights: 0,
             zobrist: 0,
             half_move_clock: 0,
-            move_counter: 0,
-            zobrist_object: Zobrist::new_zobrist(),
+            move_counter: 1,
         }
     }
 
-    fn starting_position(&self) -> Self {
+    fn starting_position() -> Self {
         let mut pos = Self::empty();
-        pos.board = START_BOARD;
+        pos.board = Self::init_board();
         pos.castling_rights = 0b1111;
-        pos.zobrist = Self::compute_zobrist(&self);
+        pos.zobrist = Self::compute_zobrist(&pos);
         pos
     }
 
-    fn piece_at() {
-        // TO-DO!
+    fn init_empty_board() -> [Cell; 120] {
+        let mut board = [Cell::Offboard; 120];
+
+        for rank in 0..8 {
+            let start = 22 + rank * 10;
+
+            for file in 0..8 {
+                board[start + file] = Cell::Empty;
+            }
+        }
+        board
     }
 
-    fn get_board() {
-        // TO-DO!
+    fn init_board() -> [Cell; 120] {
+        let mut board = Self::init_empty_board();
+
+        const BACK_RANK: [PieceKind; 8] = [
+            PieceKind::Rook,
+            PieceKind::Knight,
+            PieceKind::Bishop,
+            PieceKind::Queen,
+            PieceKind::King,
+            PieceKind::Bishop,
+            PieceKind::Knight,
+            PieceKind::Rook,
+        ];
+
+        const A1: usize = 22;
+        const A2: usize = 32;
+        const A7: usize = 82;
+        const A8: usize = 92;
+        const STRIDE: usize = 7;
+
+        for i in A2..=A2 + STRIDE {
+            board[i] = Cell::Piece(Piece::new(Color::White, PieceKind::Pawn));
+        }
+        for (i, kind) in BACK_RANK.iter().enumerate() {
+            board[i + A1] = Cell::Piece(Piece::new(Color::White, *kind));
+        }
+        for i in A7..=A7 + STRIDE {
+            board[i] = Cell::Piece(Piece::new(Color::Black, PieceKind::Pawn));
+        }
+        for (i, kind) in BACK_RANK.iter().enumerate() {
+            board[i + A8] = Cell::Piece(Piece::new(Color::Black, *kind));
+        }
+        board
     }
 
-    fn set_castling_rights() {
-        // TO-DO!
+    // Maybe this belongs to movegen
+    fn piece_at(&self, square: Square) -> Option<Piece> {
+        match self.board[square as usize] {
+            Cell::Piece(piece) => Some(piece),
+            Cell::Empty => None,
+            Cell::Offboard => None,
+        }
     }
 
-    fn check_en_passant() {
-        // TO-DO!
+    // Maybe this belongs to movegen
+    fn king_square(&self, color: Color) -> Square {
+        for (i, maybe_piece) in self.board.iter().enumerate() {
+            if let Cell::Piece(piece) = maybe_piece {
+                if piece.color == color && piece.kind == PieceKind::King {
+                    return i as u8;
+                }
+            }
+        }
+        unreachable!("no king of {:?} found on the board", color);
     }
 
+    // Maybe this belongs to movegen
+    fn set_piece(&mut self, piece: Piece, square: Square) {
+        self.board[square as usize] = Cell::Piece(piece);
+    }
+
+    // computes a hash-value for every Piece on the Board, Player to Move, Castling rights
+    // and en-passant-row. So every Boardstate has a unique hash-value
     fn compute_zobrist(&self) -> u64 {
         let mut zobrist: u64 = 0;
-        for (square, maybe_piece) in self.board.iter().enumerate() {
-            if let Some(piece) = maybe_piece {
-                zobrist ^= self.zobrist_object.zobrist_values[piece.color as usize]
-                    [piece.kind as usize][square];
+
+        // Hashvalue for every Piece on the Board
+        for (sq120, cell) in self.board.iter().enumerate() {
+            let sq64 = SQUARE120_TO_SQUARE64[sq120];
+            if sq64 < 0 {
+                continue;
+            }
+
+            if let Cell::Piece(piece) = cell {
+                zobrist ^= ZOBRIST.zobrist_values[piece.color.idx() as usize]
+                    [piece.kind.idx() as usize][sq64 as usize];
             }
         }
 
-        if self.player_to_move == Color::White {
-            zobrist ^= self.zobrist_object.zobrist_side_to_move
+        // Hashvalue for player turn
+        if self.player_to_move == Color::Black {
+            zobrist ^= ZOBRIST.zobrist_side_to_move
         }
 
-        // castling fehlt
-        // en passant fehlt
+        const CASTLING_POSSIBILITIES: [u8; 4] = [
+            0b0001, // White 0-0
+            0b0010, // White 0-0-0
+            0b0100, // Black 0-0
+            0b1000, // Black 0-0-0
+        ];
+
+        // Hashvalue for castling possibilities
+        for (i, value) in CASTLING_POSSIBILITIES.iter().enumerate() {
+            if Self::bitmask(self.castling_rights, *value) {
+                zobrist ^= ZOBRIST.zobrist_castling[i];
+            }
+        }
+
+        // Hashvalue for en passant
+        if let Some(ep_sq120) = self.en_passant_square {
+            let sq64 = SQUARE120_TO_SQUARE64[ep_sq120 as usize];
+            if sq64 >= 0 {
+                let file = (sq64 as usize) % 8;
+                zobrist ^= ZOBRIST.zobrist_enpassant[file];
+            }
+        }
 
         zobrist
+    }
+
+    // Helperfunktion that checks if a single bit in mask is also set in to_check
+    fn bitmask(to_check: u8, mask: u8) -> bool {
+        to_check & mask != 0
     }
 }
