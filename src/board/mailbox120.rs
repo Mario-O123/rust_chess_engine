37a601005a168e53012e9e486011f0f1db816b0c
Mario-O123 <M.Orsolic@campus.lmu.de>
2025-12-14 19:13:00 +0100

    Refine mailbox120.rs after first review feedback

--- src/board/mailbox120.rs
+++ src/board/mailbox120.rs
@@ -4,7 +4,7 @@ pub const OFFBOARD: i8 = -1;
 //Directions/Movement offsets, handle King and Pawn separately
 pub const ROOK_DIRECTIONS: [i8; 4] = [1, -1, 10, -10];
 pub const BISHOP_DIRECTIONS: [i8; 4] = [9, -9, 11, -11];
-pub const QUEEEN_DIRECTIONS: [i8; 8] = [1, -1, 9, -9, 10, -10, 11, -11];
+pub const QUEEN_DIRECTIONS: [i8; 8] = [1, -1, 9, -9, 10, -10, 11, -11];
 pub const KNIGHT_DIRECTIONS: [i8; 8] = [8, -8, 12, -12, 19, -19, 21, -21];
 
 //used in the static mapping as helper
@@ -15,38 +15,39 @@ pub const fn square120_from_file_rank(file: usize, rank: usize) -> usize{
 
 //this will be used later and get called many times in movegen, so inline
 pub fn is_on_board(square120: usize) -> bool {
+    square120 < BOARD_SIZE &&
     SQUARE120_TO_SQUARE64[square120] != OFFBOARD
 }
 
 //the actual mappings for reading from and into the internal engine:
 //here, we need i8 for the offboard markings
 pub static SQUARE120_TO_SQUARE64: [i8; BOARD_SIZE] = {
-    let mut map = [OFFBOARD; BOARD_SIZE];
+    let mut lookup = [OFFBOARD; BOARD_SIZE];
     let mut square64: usize = 0;
 
     for rank in 0..8 {
         for file in 0..8 {
             let square120: usize = square120_from_file_rank(file, rank);
-            map[square120] = square64;
+            lookup[square120] = square64 as i8;
             square64 += 1;
         }
     }
 
-    map
+    lookup
 };
 
 pub static SQUARE64_TO_SQUARE120: [usize; 64] = {
-    let mut map = [0usize; 64];
+    let mut lookup = [0usize; 64];
     let mut square64: usize = 0;
 
     for rank in 0..8 {
         for file in 0..8 {
             let square120 = square120_from_file_rank(file, rank);
-            map[square64] = square120;
+            lookup[square64] = square120;
             square64 += 1;
         }
     }
-    map
+    lookup
 };
 
 
