81235ae99fe6a6bbce3018065bfb55b0b863a38c
Mario-O123 <M.Orsolic@campus.lmu.de>
2025-12-18 17:47:13 +0100

    Update lib exports and refine mailbox120 after review

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod board;
+pub mod bot;
+pub mod engine;
+pub mod evaluate;
+pub mod movegen;
+pub mod position;
+pub mod search;
+pub mod utils;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
