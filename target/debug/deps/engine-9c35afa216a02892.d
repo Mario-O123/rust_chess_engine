ff460849b99849a39526ca55a10e59e8e946e14b
Mario-O123 <M.Orsolic@campus.lmu.de>
2025-12-20 15:28:10 +0100

    Clean PR: keep only debug.rs and remove target

--- target/debug/deps/engine-9c35afa216a02892.d
+++
@@ -1,5 +0,0 @@
-/home/maju/RUST/rust_chess_engine/target/debug/deps/engine-9c35afa216a02892.d: src/bin/engine.rs
-
-/home/maju/RUST/rust_chess_engine/target/debug/deps/libengine-9c35afa216a02892.rmeta: src/bin/engine.rs
-
-src/bin/engine.rs:
--- target/debug/deps/rust_chess_engine-2b95873e4dacc8d1.d
+++
@@ -1,5 +0,0 @@
-/home/maju/RUST/rust_chess_engine/target/debug/deps/rust_chess_engine-2b95873e4dacc8d1.d: src/lib.rs
-
-/home/maju/RUST/rust_chess_engine/target/debug/deps/librust_chess_engine-2b95873e4dacc8d1.rmeta: src/lib.rs
-
-src/lib.rs:
