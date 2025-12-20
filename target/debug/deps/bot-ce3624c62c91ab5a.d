16a9974e8f658f43f09d79d71d09136f59a2b138
Mario-O123 <M.Orsolic@campus.lmu.de>
2025-12-20 15:28:10 +0100

    Clean PR: keep only debug.rs and remove target

--- target/debug/deps/bot-ce3624c62c91ab5a.d
+++
@@ -1,5 +0,0 @@
-/home/maju/RUST/rust_chess_engine/target/debug/deps/bot-ce3624c62c91ab5a.d: src/bin/bot.rs
-
-/home/maju/RUST/rust_chess_engine/target/debug/deps/libbot-ce3624c62c91ab5a.rmeta: src/bin/bot.rs
-
-src/bin/bot.rs:
