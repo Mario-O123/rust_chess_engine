ab2d1ecf9a556525d9eb5cad82da086931437ad8
Emil Sitka <emil.sitka@hotmail.com>
2025-12-27 21:22:17 +0100

    corrected Bugs in movegen, switched to better Move struct, worked on legal_move_filter

--- src/movegen/pawn.rs
+++ src/movegen/pawn.rs
@@ -6,41 +6,41 @@
 
 
     fn gen_pawn_moves(position: &Position, moves: &mut Vec<Move>, square: usize,  last_move: Move) {
-        let pawn_starts_white: [usize;8] = []; //alle indexe des startrangs weißer pawns
-        let pawn_starts_black: [usize;8] = []; //alle indexe des startrangs scharzer pawns
-        let pawn_promotion_rank_white: [usize;8] = [];
-        let pawn_promotion_rank_black: [usize;8] = [];
+        let pawn_starts_white: [usize;8] = [81,82,83,84,85,86,87,88]; //alle indexe des startrangs weißer pawns
+        let pawn_starts_black: [usize;8] = [31,32,33,34,35,36,37,38]; //alle indexe des startrangs scharzer pawns
+        let pawn_promotion_rank_white: [usize;8] = [21,22,23,24,25,26,27,28];
+        let pawn_promotion_rank_black: [usize;8] = [91,92,93,94,95,96,97,98];
         if let Cell::Piece(piece) = position.board[square] {
         match piece.color {
         Color::White => {
         if pawn_starts_white.contains(square) && position.board[(square as i8 +10) as usize]== Cell::Empty && 
         position.board[(square as i8 +20) as usize ]== Cell::Empty{
             //push 2 up move to vector
-            moves.push(Move::new(square, (square+20), promotion_none , double_move_pawn_flag));
+            moves.push(Move::new(square, (square+20)));
         }
         if position.board[(square as i8 +10) as usize] == Cell::Empty {
             //push 1 up move to vector
             if pawn_promotion_rank_white.contains(square+10) {
-                moves.push(Move::new(square as u8, (square as i8 +10) as u8, promotion_knight , none_flag));
-                moves.push(Move::new(square as u8, (square as i8 +10) as u8, promotion_bishop , none_flag));
-                moves.push(Move::new(square as u8, (square as i8 +10) as u8, promotion_rook , none_flag));
-                moves.push(Move::new(square as u8, (square as i8 +10) as u8, promotion_queen , none_flag));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +10) as u8, PromotionPiece::Knight));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +10) as u8, PromotionPiece::Bishop));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +10) as u8, PromotionPiece::Rook));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +10) as u8, PromotionPiece::Queen));
 
             }else {
-                moves.push(Move::new(square as u8, (square as i8 +10) as u8, promotion_none , none_flag));
+                moves.push(Move::new(square as u8, (square as i8 +10) as u8));
             }
         }
         if let Cell::Piece(piece) = position.board[(square as i8  + 11) as usize] {
             if piece.color == Color::Black {
                 // push take right to vector
                 if pawn_promotion_rank_white.contains((square as i8 +11) as usize) {
-                    moves.push(Move::new(square as u8, (square as i8 +11) as u8, promotion_knight , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 +11) as u8, promotion_bishop , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 +11) as u8, promotion_rook , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 +11) as u8, promotion_queen , none_flag));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +11) as u8, PromotionPiece::Knight));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +11) as u8, PromotionPiece::Bishop));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +11) as u8, PromotionPiece::Rook));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +11) as u8, PromotionPiece::Queen));
 
                 }else {
-                    moves.push(Move::new(square as u8 , (square as i8 +11) as u8, promotion_none , none_flag));
+                    moves.push(Move::new(square as u8 , (square as i8 +11) as u8));
                 }
 
         }}
@@ -48,13 +48,13 @@
             if piece.color == Color::Black {
                 // push take left to vector
                 if pawn_promotion_rank_white.contains((square as i8 +9) as usize) {
-                    moves.push(Move::new(square as u8, (square as i8 +9) as u8, promotion_knight , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 +9) as u8, promotion_bishop , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 +9) as u8, promotion_rook , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 +9) as u8, promotion_queen , none_flag));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +9) as u8, PromotionPiece::Knight));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +9) as u8, PromotionPiece::Bishop));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +9) as u8, PromotionPiece::Rook));
+                moves.push(Move::new_promotion(square as u8, (square as i8 +9) as u8, PromotionPiece::Queen));
 
                 }else {
-                    moves.push(Move::new(square as u8, (square as i8 +9) as u8, promotion_none , none_flag));
+                    moves.push(Move::new(square as u8, (square as i8 +9) as u8));
                 }
         }}
         //make en passant movelogic here with last move and chek if last move 
@@ -64,45 +64,45 @@
         if pawn_starts_black.contains(square) && position.board[(square as i8 -10) as usize ]== Cell::Empty && 
         position.board[(square as i8 -20) as usize ]== Cell::Empty{
             //push 2 up move to vector
-            moves.push(Move::new(square as u8, (square as i8 -20) as u8, promotion_none , double_move_pawn_flag));
+            moves.push(Move::new(square as u8, (square as i8 -20) as u8));
 
         }
         if position.board[(square as i8 -10) as usize] == Cell::Empty {
             //push 1 up move to vector
             if pawn_promotion_rank_black.contains((square as i8 -10)as usize) {
-                moves.push(Move::new(square as u8, (square as i8 -10) as u8, promotion_knight , none_flag));
-                moves.push(Move::new(square as u8, (square as i8 -10) as u8, promotion_bishop, none_flag));
-                moves.push(Move::new(square as u8, (square as i8 -10) as u8, promotion_rook , none_flag));
-                moves.push(Move::new(square as u8, (square as i8 -10) as u8, promotion_queen , none_flag));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -10) as u8, PromotionPiece::Knight));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -10) as u8, PromotionPiece::Bishop));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -10) as u8, PromotionPiece::Rook));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -10) as u8, PromotionPiece::Queen));
 
             }else {
-                moves.push(Move::new(square, (square-10), promotion_none , none_flag));
+                moves.push(Move::new(square, (square-10)));
             }
         }
         if let Cell::Piece(piece) = position.board[(square as i8 - 11) as usize] {
             if piece.color == Color::White {
                 // push take right to vector
                 if pawn_promotion_rank_black.contains((square as i8 -11) as usize) {
-                    moves.push(Move::new(square as u8, (square as i8 -11) as u8, promotion_knight , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 -11) as u8, promotion_bishop , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 -11) as u8, promotion_rook , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 -11) as u8, promotion_queen , none_flag));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -11) as u8, PromotionPiece::Knight));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -11) as u8, PromotionPiece::Bishop));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -11) as u8, PromotionPiece::Rook));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -11) as u8, PromotionPiece::Queen));
 
                 }else {
-                    moves.push(Move::new(square as u8, (square as i8 -11) as u8, promotion_none , none_flag));
+                    moves.push(Move::new(square as u8, (square as i8 -11) as u8));
                 }
         }}
         if let Cell::Piece(piece) = position.board[(square as i8  - 9) as usize] {
             if piece.color == Color::White {
                 // push take left to vector
                 if pawn_promotion_rank_black.contains(square-9) {
-                    moves.push(Move::new(square as u8, (square as i8 -9) as u8, promotion_knight , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 -9) as u8, promotion_bishop , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 -9) as u8, promotion_rook , none_flag));
-                    moves.push(Move::new(square as u8, (square as i8 -9) as u8, promotion_queen , none_flag));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -9) as u8, PromotionPiece::Knight));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -9) as u8, PromotionPiece::Bishop));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -9) as u8, PromotionPiece::Rook));
+                moves.push(Move::new_promotion(square as u8, (square as i8 -9) as u8, PromotionPiece::Queen));
 
                 }else {
-                    moves.push(Move::new(square as u8, (square as i8 -9) as u8, promotion_none , none_flag));
+                    moves.push(Move::new(square as u8, (square as i8 -9) as u8));
                 }
         }}
             
@@ -112,34 +112,37 @@
     }
     //2moveflag muss noch ein int sein der in flags für 2move steht
     fn en_passant_moves(position: &Position, moves: &mut Vec<Move>, square: usize, last_move: Move) {
+        if last_move.is_null() {
+            return;
+        }
         if let Cell::Piece(piece) = position.board[square] {
         if let Cell::Piece(last_move_piece) = position.board[last_move.to] {
         match piece.color {
         Color::White => {
 
         if last_move_piece.kind == PieceKind::Pawn && 
-            last_move.flags == double_move_pawn_flag && 
+            last_move.to == (last_move.from -20) && 
             last_move.to == (square as i8 +1) as u8  {
-                moves.push(Move::new(square as u8, (square as i8 +11) as u8, promotion_none, en_passant_flag))
+                moves.push(Move::new_en_passant(square as u8, (square as i8 +11) as u8));
 
             }
         else if last_move_piece.kind == PieceKind::Pawn && 
-            last_move.flags == double_move_pawn_flag && 
+            last_move.to == (last_move.from -20) && 
             last_move.to == (square as i8 -1) as u8  {
-                moves.push(Move::new(square as u8 , (square as i8 +9) as u8, promotion_none, en_passant_flag))
+                moves.push(Move::new_en_passant(square as u8, (square as i8 +9) as u8));
             }
         }
         Color::Black => {
             if last_move_piece.kind == PieceKind::Pawn && 
-            last_move.flags == double_move_pawn_flag && 
+            last_move.to == (last_move.from +20) && 
             last_move.to == (square as i8 +1) as u8  {
-                moves.push(Move::new(square as u8, (square as i8 -9) as u8 , promotion_none, en_passant_flag))
+                moves.push(Move::new_en_passant(square as u8, (square as i8 -11) as u8));
 
             }
         else if last_move_piece.kind == PieceKind::Pawn && 
-            last_move.flags == double_move_pawn_flag && 
+            last_move.to == (last_move.from +20) && 
             last_move.to == (square as i8 -1) as u8  {
-                moves.push(Move::new(square as u8, (square as i8 -11) as u8, promotion_none, en_passant_flag))
+                moves.push(Move::new_en_passant(square as u8, (square as i8 -9) as u8));
             }
 
         }
--- src/movegen/piece.rs
+++ src/movegen/piece.rs
@@ -5,17 +5,22 @@
 
 fn gen_sliding_moves(position: &Position, moves: &mut Vec<Move> , square: usize , piece_offsets: &[i8] ) {
      for offset in piece_offsets {
-                    target = square as i8 + offset;
-                    while position.board[target as usize] != Cell::Offboard {
-                        if target == Cell::Empty {
+                    let mut target = square as i8 + offset;
+                    if target < 0 {
+                        continue;
+                    }
+                    while target >= 0 && position.board[target as usize] != Cell::Offboard  {
+                        if position.board[target as usize] == Cell::Empty {
                             // push move 
-                            moves.push(Move::new(square as u8 , target as u8 , promotion_none , none_flag));
+                            moves.push(Move::new(square as u8 , target as u8));
                             target += offset;
                         }else if matches!((position.board[target], position.board[square]), (Cell::Piece(slider_target), Cell::Piece(slider_square)) 
                                  if slider_target.color == slider_square.color.opposite()){
                             //push move with capture
-                            moves.push(Move::new(square  as u8, target as u8 , promotion_none , none_flag));
+                            moves.push(Move::new(square as u8 , target as u8));
 
+                            break;
+                        }else {
                             break;
                         }
                 }
@@ -25,14 +30,20 @@ fn gen_sliding_moves(position: &Position, moves: &mut Vec<Move> , square: usize 
 //hier könnte man zeilen sparen anstatt für empty und enemy piece zu cheken einen check für nicht offboard oder ma lässt so wenn man noch ne capture flag will
 fn gen_jumping_moves(position: &Position, moves: &mut Vec<Move> , square: usize , piece_offsets: &[i8] ) {
     for offset in piece_offsets {
+                if (square as i8 + offset) < 0 {
+                    continue;
+                }
                 if position.board[(square as i8 + offset) as usize] == Cell::Empty {
                     //push move to vector
-                    moves.push(Move::new(square as u8, (square as i8+ offset) as u8 , promotion_none , none_flag));
+                    moves.push(Move::new(square as u8, (square as i8+ offset) as u8));
 
                 }else if matches!((position.board[(square as i8+ offset)as usize ], position.board[square]), (Cell::Piece(jumper_target), Cell::Piece(jumper_square)) 
                          if jumper_target.color == jumper_square.color.opposite()) {
                         //push move to vector with capture flag
-                        moves.push(Move::new(square as u8, (square + offset) as u8, promotion_none , none_flag));
+                        moves.push(Move::new(square as u8, (square + offset) as u8));
+
+                }
+                else {
 
                 }
             }
@@ -40,29 +51,30 @@ fn gen_jumping_moves(position: &Position, moves: &mut Vec<Move> , square: usize 
 }
 
 // hier braucht man color castling rights struct aus gamestate und dann noch schauen ob die felder zwischen king und rook None sind
+//hier eventuell nur legal moves generieeren noch machen
 fn gen_castling_moves(position : &Position , moves: &mut Vec<Move> , square: usize) {
 match position.board[square] {
-    Cell::Piece(Piece { color : Color::White }) => {
+    Cell::Piece(Piece { color : Color::White , kind : PieceKind::King}) => {
         if position.castling_rights & 0b0010 !=0 && position.board[92]==Cell::Empty && position.board[93]==Cell::Empty && position.board[94]==Cell::Empty
         && matches!(position.board[91], Cell::Piece(Piece {kind: PieceKind::Rook, ..})) {
-            moves.push(Move::new(square as u8, 93 , promotion_none , castling_flag));
+            moves.push(Move::new_castling(square as u8, 93));
 
         } 
         if position.castling_rights & 0b0001 !=0 && position.board[96]==Cell::Empty && position.board[97]==Cell::Empty
-        && && matches!(position.board[98], Cell::Piece(Piece {kind: PieceKind::Rook, ..})){
-            moves.push(Move::new(square as u8, 97 , promotion_none , castling_flag));
+        && matches!(position.board[98], Cell::Piece(Piece {kind: PieceKind::Rook, ..})){
+            moves.push(Move::new_castling(square as u8, 97));
 
         }
     }
-    Color::Black => {
+    Cell::Piece(Piece {color : Color::Black , kind : PieceKind::King}) => {
         if position.castling_rights & 0b1000 !=0 && position.board[22]== Cell::Empty && position.board[23] == Cell::Empty && position.board[24]==Cell::Empty 
-        && && matches!(position.board[21], Cell::Piece(Piece {kind: PieceKind::Rook, ..})){
-            moves.push(Move::new(square as u8, 23 , promotion_none , castling_flag));
+        && matches!(position.board[21], Cell::Piece(Piece {kind: PieceKind::Rook, ..})){
+            moves.push(Move::new_castling(square as u8, 23 ));
 
         }
         if position.castling_rights & 0b0100 !=0  && position.board[26]==Cell::Empty && position.board[27]==Cell::Empty 
-        && && matches!(position.board[28], Cell::Piece(Piece {kind: PieceKind::Rook, ..})){
-            moves.push(Move::new(square as u8, 27 , promotion_none , castling_flag));
+        && matches!(position.board[28], Cell::Piece(Piece {kind: PieceKind::Rook, ..})){
+            moves.push(Move::new_castling(square as u8, 27));
 
         }
     }
