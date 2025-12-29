9bbc02ceef1b521f72773c71460b158edfe905c4
Julien0905 <juju.kriebel11@gmail.com>
2025-12-29 15:20:47 +0100

    feedback paper implemented till 4.4 perft

--- src/movegen/legal_move_filter.rs
+++ src/movegen/legal_move_filter.rs
@@ -1,17 +1,19 @@
+use crate::position::Position;
+use crate::movegen::Move;
+use crate::movegen::attack::is_in_check;
 
 
-
-fn legal_move_filter(position: &Position , moves : &[Move]) -> Vec<Move> {
+fn filter_legal_moves(position: &Position , moves : &[Move]) -> Vec<Move> {
 
    let mut legal_moves = Vec::new();
 
-    for &pseudo_move in moves {
+    for mv in moves.iter().copied() {
         let mut new_pos = position.clone();
-        new_pos.make_move(pseudo_move);
+        new_pos.make_move(mv);
 
         // Check if own King is in check
         if !is_in_check(&new_pos, position.player_to_move) {
-            legal_moves.push(pseudo_move);
+            legal_moves.push(mv);
         }
     }
 
