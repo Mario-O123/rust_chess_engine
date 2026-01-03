f38d982c48c147f5027138e1c8e70fc56cac0991
Emil Sitka <emil.sitka@hotmail.com>
2026-01-03 19:37:53 +0100

    fix(movegen): fixed some stuff in pawn, piece , removed pin.rs

--- src/movegen/piece.rs
+++ src/movegen/piece.rs
@@ -72,89 +72,76 @@ pub fn gen_jumping_moves(
 
 // dsquare indexes are magic numbers should be changed if wrong or hard to understand
 
-pub fn gen_castling_moves(position: &Position, moves: &mut Vec<Move>, king_from: usize) {
+
+pub fn gen_castling_moves(position : &Position , moves: &mut Vec<Move> , king_from: usize) {
     match position.board[king_from] {
-        Cell::Piece(Piece {
-            color: Color::White,
-            kind: PieceKind::King,
-        }) => {
-            if position.castling_rights & 0b0010 != 0
-                && position.board[92] == Cell::Empty
-                && position.board[93] == Cell::Empty
-                && position.board[94] == Cell::Empty
-                && matches!(
-                    position.board[91],
-                    Cell::Piece(Piece {
-                        kind: PieceKind::Rook,
-                        color: Color::White
-                    })
-                )
-                && is_square_attacked(position, king_from, Color::Black) == false
-                && is_square_attacked(position, 94, Color::Black) == false
-                && is_square_attacked(position, 93, Color::Black) == false
-                && is_square_attacked(position, 92, Color::Black) == false
-                && is_square_attacked(position, 91, Color::Black) == false
-            {
-                moves.push(Move::new_castling(king_from, 93));
-            }
-            if position.castling_rights & 0b0001 != 0
-                && position.board[96] == Cell::Empty
-                && position.board[97] == Cell::Empty
-                && matches!(
-                    position.board[98],
-                    Cell::Piece(Piece {
-                        kind: PieceKind::Rook,
-                        color: Color::White
-                    })
-                )
-                && is_square_attacked(position, king_from, Color::Black) == false
-                && is_square_attacked(position, 96, Color::Black) == false
-                && is_square_attacked(position, 98, Color::Black) == false
-                && is_square_attacked(position, 97, Color::Black) == false
-            {
-                moves.push(Move::new_castling(king_from, 97));
-            }
-        }
-        Cell::Piece(Piece {
-            color: Color::Black,
-            kind: PieceKind::King,
-        }) => {
-            if position.castling_rights & 0b1000 != 0
-                && position.board[22] == Cell::Empty
-                && position.board[23] == Cell::Empty
-                && position.board[24] == Cell::Empty
-                && matches!(
-                    position.board[21],
-                    Cell::Piece(Piece {
-                        kind: PieceKind::Rook,
-                        color: Color::Black
-                    })
-                )
-                && is_square_attacked(position, king_from, Color::White) == false
-                && is_square_attacked(position, 24, Color::White) == false
-                && is_square_attacked(position, 23, Color::White) == false
-                && is_square_attacked(position, 22, Color::White) == false
-                && is_square_attacked(position, 21, Color::White) == false
-            {
-                moves.push(Move::new_castling(king_from, 23));
-            }
-            if position.castling_rights & 0b0100 != 0
-                && position.board[26] == Cell::Empty
-                && position.board[27] == Cell::Empty
-                && matches!(
-                    position.board[28],
-                    Cell::Piece(Piece {
-                        kind: PieceKind::Rook,
-                        color: Color::Black
-                    })
-                )
-                && is_square_attacked(position, king_from, Color::White) == false
-                && is_square_attacked(position, 26, Color::White) == false
-                && is_square_attacked(position, 28, Color::White) == false
-                && is_square_attacked(position, 27, Color::White) == false
-            {
-                moves.push(Move::new_castling(king_from, 27));
-            }
+
+    Cell::Piece(Piece { 
+        color : Color::White , 
+        kind : PieceKind::King}) => {
+
+        if position.castling_rights & 0b0010 !=0 
+            && position.board[22]==Cell::Empty 
+            && position.board[23]==Cell::Empty 
+            && position.board[24]==Cell::Empty
+            && matches!(
+                position.board[21], 
+                Cell::Piece(Piece {
+                    kind: PieceKind::Rook, 
+                    color: Color::White}))
+            && is_square_attacked(position, king_from, Color::Black) == false 
+            && is_square_attacked(position, 24, Color::Black) == false
+            && is_square_attacked(position, 23, Color::Black) == false  {
+            moves.push(Move::new_castling(king_from , 23));
+
+        } 
+        if position.castling_rights & 0b0001 !=0 
+            && position.board[26]==Cell::Empty 
+            && position.board[27]==Cell::Empty
+            && matches!(
+                position.board[28], 
+                Cell::Piece(Piece {
+                    kind: PieceKind::Rook, 
+                    color: Color::White}))
+            && is_square_attacked(position, king_from, Color::Black) == false 
+            && is_square_attacked(position, 26, Color::Black) == false
+            && is_square_attacked(position, 27, Color::Black) ==false
+        {
+            moves.push(Move::new_castling(king_from, 27));
+
         }
     }
-}
+    Cell::Piece(Piece {color : Color::Black , kind : PieceKind::King}) => {
+        if position.castling_rights & 0b1000 !=0 
+            && position.board[92]== Cell::Empty 
+            && position.board[93] == Cell::Empty 
+            && position.board[94]==Cell::Empty 
+            && matches!(
+                position.board[91], 
+                Cell::Piece(Piece {
+                    kind: PieceKind::Rook, 
+                    color: Color::Black}))
+            && is_square_attacked(position, king_from, Color::White) == false 
+            && is_square_attacked(position, 94, Color::White) == false
+            && is_square_attacked(position, 93, Color::White) == false 
+        {
+            moves.push(Move::new_castling(king_from , 93 ));
+
+        }
+        if position.castling_rights & 0b0100 !=0  
+            && position.board[26]==Cell::Empty 
+            && position.board[97]==Cell::Empty 
+            && matches!(
+                position.board[98], 
+                Cell::Piece(Piece {
+                    kind: PieceKind::Rook, 
+                    color: Color::Black}))
+            && is_square_attacked(position, king_from, Color::White) == false 
+            && is_square_attacked(position, 96, Color::White) == false
+            && is_square_attacked(position, 97, Color::White) == false
+         {
+            moves.push(Move::new_castling(king_from , 97));
+
+        }
+    }
+}}
