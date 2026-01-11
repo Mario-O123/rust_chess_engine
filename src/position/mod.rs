34bdd1ff5e2579ce29bffc661f32d28a2c8a6394
Mario-O123 <M.Orsolic@campus.lmu.de>
2026-01-11 22:52:50 +0100

    style(position/mod): run cargo fmt to satisfy fmt check

--- src/position/mod.rs
+++ src/position/mod.rs
@@ -3,5 +3,5 @@ pub mod game;
 pub mod position;
 pub mod state;
 
-pub use position::{Cell, Color, Piece, Position, Square, PieceKind}; //deleted BOARD120, constant import is private, added PieceKind
+pub use position::{Cell, Color, Piece, PieceKind, Position, Square}; //deleted BOARD120, constant import is private, added PieceKind
 pub use state::{GameState, State, Undo};
