d6c1b83036c7413ae96572b25f2bd3b36b0f3f80
Julien0905 <juju.kriebel11@gmail.com>
2026-02-26 21:28:27 +0100

    doc(bin/bot): added some comments and documentation

--- src/bin/bot.rs
+++ src/bin/bot.rs
@@ -2,22 +2,21 @@ use anyhow::Result;
 
 use rust_chess_engine::bot::{BotConfig, LichessBot};
 
+//main function to run with lichess 
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Starting Lichess Bot...");
     dotenv::dotenv().ok();
 
-    // Load configuration from environment
+    // Load configuration from env
     let config = BotConfig::from_env()?;
 
     println!("Configuration:");
     println!("Engine path: {}", config.engine_path);
     println!("Move time: {}ms", config.movetime_ms);
 
-    // Initialize bot
+    
     let mut bot = LichessBot::new(config).await?;
-
-    // Run bot (blocks until error or Ctrl+C)
     bot.run().await?;
 
     Ok(())
